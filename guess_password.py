097f3dd659160d9d5cffbe22ee229a62573c8921
Eric Tang <etang619@gmail.com>
2021-06-26 14:58:55 +0800

    minor update

--- guess_password.py
+++ guess_password.py
@@ -1,12 +1,12 @@
 password = 'a123456'
 tries =3
 while tries != 0:
+	tries = tries - 1
 	guess = input('guess my password: ')
 	if guess == password:
 		print('You guess it right')
 		break
 	else:
-		tries = tries - 1
 		if tries == 0:
 			print('You have no more chances!')
 		else:
